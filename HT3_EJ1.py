94c4279f82281bf055117bc06ec989c158d15d18
luisfergr <78666654+luisfergr@users.noreply.github.com>
2021-03-12 15:39:55 -0600

    Add files via upload

---
+++ HT3_EJ1.py
@@ -0,0 +1,14 @@
+print("Validacion contraseña")
+
+key = "usac2021"
+password = input("Introduce la contraseña: ")
+print("\n")
+if key == password.lower():
+    print("La contaseña coincide")
+    #print("Contraseña registrada: "+ key + ", La contraseña ingresada: "+ password)
+else:
+    print("La contraseña no coincide")
+
+    print("\n")
+
+   
